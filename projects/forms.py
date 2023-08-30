eadb0f6bd480cbe9e5971380c085fdf6f8723f72
amine <aminekhalfaoui814@gmail.com>
2023-08-30 15:11:23 +0200

    login and logout

--- projects/forms.py
+++ projects/forms.py
@@ -1,23 +1,23 @@
 from django import forms
 from . import models
 
-attrs = {'class':'form-control'}
+attrs = {'class': 'form-control'}
 class ProjectCreateForm(forms.ModelForm):
     class Meta:
         model = models.Project
-        fields = ['category','title','description']
+        fields = ['category', 'title', 'description']
         widgets = {
-            'category':forms.Select(attrs=attrs),
-            'title':forms.TextInput(attrs=attrs),
-            'description':forms.Textarea(attrs=attrs)
+            'category': forms.Select(attrs=attrs),
+            'title': forms.TextInput(attrs=attrs),
+            'description': forms.Textarea(attrs=attrs)
         }
 
 class ProjectUpdateForm(forms.ModelForm):
     class Meta:
         model = models.Project
-        fields = ['category','title','status']
+        fields = ['category', 'title', 'status']
         widgets = {
-            'category':forms.Select(attrs=attrs),
-            'title':forms.TextInput(attrs=attrs),
-            'status':forms.Select(attrs=attrs)
+            'category': forms.Select(attrs=attrs),
+            'title': forms.TextInput(attrs=attrs),
+            'status': forms.Select(attrs=attrs)
         }
--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,9 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 from . import models
 from . import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = models.Project
     template_name = 'project/project_list.html'
     paginate_by = 6
@@ -15,25 +16,25 @@ class ProjectListView(ListView):
             where['title__icontains'] = query
         return query_set.filter(**where)
 
-class ProjectCreateView(CreateView):
+class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = models.Project
     form_class = forms.ProjectCreateForm
     template_name = 'project/project_create.html'
     success_url = reverse_lazy('project_list')
 
-class ProjectUpdateView(UpdateView):
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Project
     form_class = forms.ProjectUpdateForm
     template_name = 'project/project_update.html'
     def get_success_url(self):
         return reverse('project_update', args=[self.object.id])
 
-class ProjectDeleteView(DeleteView):
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Project
     template_name = 'project/project_delete.html'
     success_url = reverse_lazy('project_list')
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = models.Task
     fields = ['project','description']
     http_method_names = ['post']
@@ -42,7 +43,7 @@ class TaskCreateView(CreateView):
         return reverse('project_update', args=[self.object.project.id])
     #self.object.project.id t5alina n3od lsaf7et idarat al machro3
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Task
     fields = ['is_completed']
     http_method_names = ['post']
@@ -50,7 +51,7 @@ class TaskUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('project_update', args=[self.object.project.id])
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Task
     def get_success_url(self):
         return reverse('project_update', args=[self.object.project.id])
